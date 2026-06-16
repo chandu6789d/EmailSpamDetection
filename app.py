1f48bbfcf2f38f08ae27deed1d07b7763a88c1bd
chandu6789d <1009chandrakanth@gmail.com>
2026-06-16 11:15:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pickle
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import string
+ps = PorterStemmer()
+nltk.download('punkt')
+nltk.download('stopwords')
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    
+    text = y[:]
+    y.clear()
+    
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+            
+    text = y[:]
+    y.clear()
+    
+    for i in text:
+        y.append(ps.stem(i))
+    
+            
+    return " ".join(y)
+model = pickle.load(open("model2.pkl", "rb"))
+tfidf=pickle.load(open("vectorizer2.pkl", "rb"))
+st.title("Spam Detector")
+
+text = st.text_area("Enter Message")
+
+
+if st.button("Predict"):
+    msg = transform_text(text)
+    tfidf_msg = tfidf.transform([msg])
+    result = model.predict(tfidf_msg)[0]
+
+    if result == 1:
+        st.error("Spam")
+    else:
+        st.success("Not Spam")
