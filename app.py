60650ae9fa60ed8521df00289fc5f2deaee844df
chandu6789d <1009chandrakanth@gmail.com>
2026-06-16 11:29:38 +0530

    Enhance NLTK resource handling in app.py
    Added checks and downloads for additional NLTK resources.

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import pickle
 import nltk
+import nltk
+
 try:
     nltk.data.find('tokenizers/punkt')
-except:
+except LookupError:
     nltk.download('punkt')
 
 try:
+    nltk.data.find('tokenizers/punkt_tab')
+except LookupError:
+    nltk.download('punkt_tab')
+
+try:
     nltk.data.find('corpora/stopwords')
-except:
+except LookupError:
     nltk.download('stopwords')
+    
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import string
